ecc895d838435ce44f1b3fa51222c46be4adce6e
Kenneth Zheng <ken_zhe@msn.com>
2017-02-02 17:37:10 -0700

    Added 4.4 to optimal_stop.py. Added graphing and simple interface to optimal_stop.py

--- hw1/optimal_stop.py
+++ hw1/optimal_stop.py
@@ -1,3 +1,8 @@
+# Requires Python3 and and Python3 MatPlotLib library to be installed.
+
+import matplotlib.pyplot as plt
+import sys
+
 def vAlg(prob, absorbing, f, error, cost = [], discount = 1):
     """
     Calculates the vector v for optimal stopping when an sup-norm error
@@ -71,6 +76,12 @@ def srwTest():
     discount = 0.9
     print(vAlg(prob, absorbing, f, 1e-6, cost, discount))
 
+def graph(states, f, v):
+    plt.plot(states, f, label="f")
+    plt.plot(states, v, label="v")
+    plt.legend()
+    plt.show()
+
 def problem4_1():
     prob = [[0.0] * 11 for i in range(11)]
     prob[0][0] = 1.0
@@ -85,7 +96,11 @@ def problem4_1():
     absorbing.append(True)
 
     f = [0, 2, 4, 3, 10, 0, 6, 4, 3, 3, 0]
-    print(vAlg(prob, absorbing, f, 1e-6))
+    v = vAlg(prob, absorbing, f, 1e-6)
+    print(v)
+
+    states = [i for i in range(11)]
+    graph(states, f, v)
 
 def problem4_3a():
     prob = [[0.0] * 11 for i in range(11)]
@@ -105,7 +120,11 @@ def problem4_3a():
 
     f = [0, 2, 4, 3, 10, 0, 6, 4, 3, 3, 0]
     
-    print(vAlg(prob, absorbing, f, 1e-6, g))
+    v = vAlg(prob, absorbing, f, 1e-6, g)
+
+    print(v)
+    states = [i for i in range(11)]
+    graph(states, f, v)
 
 def problem4_3b():
     prob = [[0.0] * 11 for i in range(11)]
@@ -121,7 +140,11 @@ def problem4_3b():
     absorbing.append(True)
 
     f = [0, 2, 4, 3, 10, 0, 6, 4, 3, 3, 0]
-    print(vAlg(prob, absorbing, f, 1e-6, [], 0.8))
+    v = vAlg(prob, absorbing, f, 1e-6, [], 0.95)
+
+    print(v)
+    states = [i for i in range(11)]
+    graph(states, f, v)
 
 def problem4_3c():
     prob = [[0.0] * 11 for i in range(11)]
@@ -140,8 +163,11 @@ def problem4_3c():
     g.append(0.75)
 
     f = [0, 2, 4, 3, 10, 0, 6, 4, 3, 3, 0]
-    
-    print(vAlg(prob, absorbing, f, 1e-6, g, 0.8))
+   
+    v = vAlg(prob, absorbing, f, 1e-6, g, 0.95)
+    print(v)
+    states = [i for i in range(11)]
+    graph(states, f, v)
 
 def problem4_2():
     # Warning everything is zero indexed
@@ -156,6 +182,93 @@ def problem4_2():
     absorbing = [False] * 11
     absorbing[5] = True
     f = [2, 3, 4, 5, 6, 0, 8, 9, 10, 11, 12]
-    print(vAlg(prob, absorbing, f, 1e-6))
-        
+    v = vAlg(prob, absorbing, f, 1e-6)
 
+    print(v)
+    states = [i for i in range(2, 13)]
+    graph(states, f, v)
+        
+def problem4_4a():
+    # Warning everything is zero indexed
+    prob = [[0.0] * 11 for i in range(11)]
+    for i in range(11):
+        for j in range(6):
+            prob[i][j] = (j + 1.0) / 36
+
+        for j in range(5):
+            prob[i][6 + j] = (5.0 - j) / 36
+
+    absorbing = [False] * 11
+    absorbing[5] = True
+    f = [2, 3, 4, 5, 6, 0, 8, 9, 10, 11, 12]
+    g = [2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1]
+    v = vAlg(prob, absorbing, f, 1e-6, g)
+
+    print(v)
+    states = [i for i in range(2, 13)]
+    graph(states, f, v)
+
+def problem4_4b():
+    # Warning everything is zero indexed
+    prob = [[0.0] * 11 for i in range(11)]
+    for i in range(11):
+        for j in range(6):
+            prob[i][j] = (j + 1.0) / 36
+
+        for j in range(5):
+            prob[i][6 + j] = (5.0 - j) / 36
+
+    absorbing = [False] * 11
+    absorbing[5] = True
+    f = [2, 3, 4, 5, 6, 0, 8, 9, 10, 11, 12]
+
+    v = vAlg(prob, absorbing, f, 1e-6, [], 0.8)
+    print(v)
+    states = [i for i in range(2, 13)]
+    graph(states, f, v)
+        
+def problem4_4c():
+    # Warning everything is zero indexed
+    prob = [[0.0] * 11 for i in range(11)]
+    for i in range(11):
+        for j in range(6):
+            prob[i][j] = (j + 1.0) / 36
+
+        for j in range(5):
+            prob[i][6 + j] = (5.0 - j) / 36
+
+    absorbing = [False] * 11
+    absorbing[5] = True
+    f = [2, 3, 4, 5, 6, 0, 8, 9, 10, 11, 12]
+    g = [2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1]
+
+    v = vAlg(prob, absorbing, f, 1e-6, g, 0.8)
+
+    print(v)
+    states = [i for i in range(2, 13)]
+    graph(states, f, v)
+
+print("Input problem number, specifying letter if required.")
+print("For instance, to request problem 4.3a, type '4.3a' (without quotes).")
+print("To exit, enter 'n' (without quotes).")
+for line in sys.stdin:
+    if line == "4.1\n":
+        problem4_1()
+    elif line == "4.2\n":
+        problem4_2()
+    elif line == "4.3a\n":
+        problem4_3a()
+    elif line == "4.3b\n":
+        problem4_3b()
+    elif line == "4.3c\n":
+        problem4_3c()
+    elif line == "4.4a\n":
+        problem4_4a()
+    elif line == "4.4b\n":
+        problem4_4b()
+    elif line == "4.4c\n":
+        problem4_4c()
+    elif line == "n\n":
+        break
+    else:
+        print("Please try again.")
